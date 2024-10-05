a796e28b535629e31cd0b8a20a7b9642d6a93d88
Aymen Alsuhaiqi <engaymen.work@gmail.com>
2024-10-05 09:38:27 +0300

    style: add some stayle for add post page

--- post/forms.py
+++ post/forms.py
@@ -5,4 +5,18 @@ class AddPostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title','content','media']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500',
+                'placeholder': 'اكتب عنوان لمنشورك'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500',
+                'placeholder': 'محتوى المنشور',
+                'rows': 4
+            }),
+            'media': forms.ClearableFileInput(attrs={
+                'class': 'w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500'
+            })
+        }
             
